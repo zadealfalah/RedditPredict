65fc0e5777e09d407664786c3c8f94a9780aaba5
zadealfalah@gmail.com <zadealfalah@gmail.com>
2023-02-18 18:06:37 +0000

    created relevant csv - still needs yml and tests

--- models/staging/2014_comments/stg_raw_2014_comments__comments.sql
+++ models/staging/2014_comments/stg_raw_2014_comments__comments.sql
@@ -1,9 +1,43 @@
-select
-    id as comment_id,
-    author,
-    subreddit,
-    score,
-    created_utc,
-    body
-from `redditpredict`.reddit_comments_2014.full_2014_comments
+{%- set relevant_dates = ['HOUR', 'DAY', 'MONTH', 'YEAR'] -%}
+with 
 
+comments as (
+    
+    select * from {{ source('reddit_comments_2014', 'full_2014_comments') }}
+
+),
+
+split_dates as (
+
+    select  
+        id as comment_id, 
+        author,
+        subreddit,
+        score,
+        body,
+
+        {% for relevant_date in relevant_dates -%}
+            cast(extract({{relevant_date|upper}} from timestamp_micros(created_utc*1000000)) as int64) as {{relevant_date|upper}}_int,
+        {% endfor %}
+    from comments
+),
+
+-- {%- set selected_subreddit = ['funny'] -%} -- can add list, filter more for future work
+soft_deletes as (
+    select *,
+        case
+            when comment_id is null then true
+            when subreddit is null then true
+            when score is null then true
+            when body is null then true
+            when body = '[deleted]' then true
+            when body = '[removed]' then true
+            when year_int != 2014 then true
+            when subreddit != 'funny' then true -- just want funny to replicate old results now
+            else false
+        end as to_delete
+    from split_dates
+)
+
+
+select * from soft_deletes where to_delete = false
