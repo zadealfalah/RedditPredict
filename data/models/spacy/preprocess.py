41ee40d6252dd28968a545bb2846704218e104bc
zadealfalah <zadealfalah@gmail.com>
2023-03-26 05:07:56 -0500

    added project yml, began writing spacy configs

---
+++ data/models/spacy/preprocess.py
@@ -0,0 +1,10 @@
+def main(
+    input_path,
+    output_path
+    
+):
+    nlp = spacy.blank("en")
+    doc_bin = DocBin()
+    data_tuples = 
+    for doc, comm in nlp.pipe(data_tuples, as_tuples=True):
+        if comm['']
